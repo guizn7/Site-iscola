a750262e37984cc7fa9bdeb87c8fff415b046a25
guizn7 <160601652+guizn7@users.noreply.github.com>
2024-10-08 14:22:31 -0300

    Add files via upload

---
+++ projeto1 3 tri/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Química',
+    'Quais são os tipos possíveis de cadeias?',
+    'Abertas, fechadas e mistas.'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma cadeia saturada?',
+    'Com apenas ligação simples entre C.'
+)
+
+criaCartao(
+    'Química',
+    'Prefixo na nomenclatura dos compostos orgânicos?',
+    'De acordo com o n de carbonos 1C - met 2C - et 3C - prop 4C - but')
+
+criaCartao(
+    'Química',
+    'O que é uma cadeia heterogênea?',
+    'Cadeia com pelo menos um átomo diferente de C entre carbonos.'
+)
